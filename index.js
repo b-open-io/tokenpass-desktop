2d4cbae0feaf31432d39ac14491bcee80ec83010
Satchmo <rohenaz@gmail.com>
2026-01-16 18:32:13 -0500

    Upgrade Electron and rebrand to TokenPass
    - Upgrade Electron 10.1.3 → 27.3.11
    - Convert to ES modules
    - Remove starfish-server dependency
    - Update open package to v11
    - Rename from starfish to tokenpass
    - Update appId to app.tokenpass

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const {app, Menu, Tray } = require('electron')
-const starfish = require('starfish-server')
-const open = require('open')
+import { app, Menu, Tray, nativeImage } from 'electron'
+import open from 'open'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 let tray = null
-const init = () => {
-  const homedir = require('os').homedir();
-  starfish({ db: homedir + "/.starfish" })
-  createTray()
-  open("http://localhost:21000")
-}
+
 const createTray = () => {
-  tray = new Tray(process.resourcesPath + "/extraResources/icon.png")
+  const iconPath = process.resourcesPath
+    ? join(process.resourcesPath, 'extraResources', 'icon.png')
+    : join(__dirname, 'extraResources', 'icon.png')
+
+  const icon = nativeImage.createFromPath(iconPath)
+  tray = new Tray(icon)
+
   const contextMenu = Menu.buildFromTemplate([{
     label: 'Dashboard',
     click: () => {
-      open("http://localhost:21000")
+      open('http://localhost:21000')
     }
   }, {
     label: 'Exit',
@@ -21,20 +27,32 @@ const createTray = () => {
       app.quit()
     }
   }])
+
   if (process.platform === 'win32') {
     tray.on('click', () => {
       tray.popUpContextMenu(contextMenu)
     })
   }
-  tray.setToolTip('Starfish')
+
+  tray.setToolTip('TokenPass')
   tray.setContextMenu(contextMenu)
 }
+
+const init = async () => {
+  createTray()
+  await open('http://localhost:21000')
+}
+
 app.whenReady().then(() => {
   init()
 })
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-if (app.dock) app.dock.hide()
+
+if (app.dock) {
+  app.dock.hide()
+}
